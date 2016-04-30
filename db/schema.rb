05c892a2092a23e43d2735a7e66933c4df7ad75a
Bryan Martinez <BryanMartinezDev@gmail.com>
2016-04-29 20:12:28 -0400

    Validations are up and runninggit statusgit status

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429214144) do
+ActiveRecord::Schema.define(version: 20160429234817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price_in_cents"
+    t.string   "name",           null: false
+    t.string   "description",    null: false
+    t.integer  "price_in_cents", null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
