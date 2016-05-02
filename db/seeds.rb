c9400687fab5397287d4f4c98a9dbcb239cfade5
Bryan Martinez <BryanMartinezDev@gmail.com>
2016-05-01 20:33:43 -0400

    seedssss and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,15 @@ bryan = User.create(first_name: "Bryan", last_name: "Martinez", email: "bryan@gm
 serena = User.create(first_name: "Serena", last_name: "Stater", email: "serena@gmail.com", password: "qwerty", role: "admin")
 
 dracula = User.create(first_name: "Vlad", last_name: "Dracula", email: "vlad@gmail.com", password: "qwerty")
+
+racoon = Product.create(name: "Like a Sir", description: "Dress your racoon like a Sir!", image: "http://imgur.com/wm8NL2b", price_in_cents: "2999")
+
+cat1 = Product.create(name: "Lobster", description: "Dress your cat like a lobster!", image: "http://imgur.com/pTHB6A1", price_in_cents: "2999")
+
+cat2 = Product.create(name: "Sushi", description: "Dress your cat like a sushi!", image: "http://imgur.com/IohSsS3", price_in_cents: "2999")
+
+cat3 = Product.create(name: "Overall", description: "Dress your cat like in an overall!", image: "http://imgur.com/TWGHeYz", price_in_cents: "2999")
+
+duck = Product.create(name: "Ugly swan no more", description: "Dress up that duck like a princess", image: "http://imgur.com/6ZFtBFN", price_in_cents: "3999")
+
+hedgehog = Product.create(name: "Dracula's only friend", description: "Dress your hedgehog like the Count's partner in crime", image: "http://imgur.com/UsDv6WX", price_in_cents: "4999")
