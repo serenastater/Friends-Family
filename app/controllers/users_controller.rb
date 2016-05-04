431ea68ffbd12b48f9fe66b2f49fa9032a6a3dee
marcemc <marcemc@yahoo.com>
2016-05-04 16:51:14 -0400

    fixed signup view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_admin, only: [:update, :destroy, :new]
+  before_action :require_admin, only: [:update, :destroy]
   # before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
